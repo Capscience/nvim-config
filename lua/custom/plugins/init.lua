b719c4d2ac3bfd44d58d224d763188148e88466d
Simo Soini <simo@capscience.fi>
2025-06-05 16:37:38 +0300

    Add neorg plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -48,6 +48,32 @@ return {
     end,
   },
 
+  {
+    'nvim-neorg/neorg',
+    lazy = false,
+    version = '*',
+    config = function()
+      require('neorg').setup {
+        load = {
+          ['core.defaults'] = {},
+          ['core.concealer'] = {},
+          ['core.dirman'] = {
+            config = {
+              workspaces = {
+                notes = '~/notes',
+              },
+              default_workspace = 'notes',
+            },
+          },
+          ['core.summary'] = {},
+        },
+      }
+
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  },
+
   -- INFO: Previously used, now disabled plugins
 
   -- Navigation integration for tmux
